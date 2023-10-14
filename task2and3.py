78837b68494068402ad710f63a6583609f7ad1e9
getuarkelmendi6 <getuarkelmendi6@gmail.com>
2023-10-14 22:52:41 +0200

    Task 2 and 3 from Lesson 2

---
+++ task2and3.py
@@ -0,0 +1,43 @@
+# Ky program shfaq disa mesazhe të ndryshme përmes funksionit print.
+
+# Shfaq një mesazh të thjeshtë
+print("Ky është një mesazh i thjeshtë.")
+
+# Shfaq disa numra të ndryshëm me ndarjen nga një presje dhe duke përdorur 'sep'
+print("Numrat e parë janë:", 1, 2, 3, 4, sep=', ')
+
+# Shfaq një mesazh pa linjë të re pas ekzekutimit (pa 'end')
+print("Ky mesazh do të shfaqet pa linjë të re të re pas tij.", end='')
+
+# Shfaq një mesazh tjetër pas mesazhit të mëparshëm, pa linjë të re në mes
+print("Ky mesazh do të shfaqet në të njëjtën linjë me mesazhin e mëparshëm.")
+
+# Shfaq një mesazh të fundit
+print("Ky është mesazhi i fundit.")
+
+
+
+
+#Task 3 included in this file
+
+print(
+"""
+#########
+#       #
+#       #
+#       #
+#########
+""")
+
+
+print(
+"""
+#       #
+#       #
+######### 
+#       # 
+#       #
+""")
+
+
+
