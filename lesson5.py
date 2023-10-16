d402a01736123f204aae40ae95f9ba5564367ea5
getuarkelmendi6 <getuarkelmendi6@gmail.com>
2023-10-16 21:09:01 +0200

    Task 1 and 2

---
+++ lesson5.py
@@ -0,0 +1,21 @@
+# Task 1
+
+import random
+
+random_num = random.randint(1,10)
+
+print("The random number generated is: ", random_num)
+
+# Task 2
+
+name = input("Name: ")
+age = input("Age: ")
+age = int(age)
+
+print(f"Hello {name}, on your next birthday you’ll be {age+1} years")
+
+# -> To complete
+
+# Task 3
+
+# Task 3
