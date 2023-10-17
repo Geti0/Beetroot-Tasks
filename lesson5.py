70f8c9003c24907fb1ac33a9dac0549faff6def1
getuarkelmendi6 <getuarkelmendi6@gmail.com>
2023-10-17 23:43:04 +0200

    Task 1,2,3,4

--- lesson5.py
+++ lesson5.py
@@ -1,4 +1,4 @@
-# Task 1
+# # Task 1
 
 import random
 
@@ -14,8 +14,56 @@ age = int(age)
 
 print(f"Hello {name}, on your next birthday you’ll be {age+1} years")
 
-# -> To complete
 
 # Task 3
 
-# Task 3
+import random
+
+def generate_random_string(input_string):
+    # Convert the input string to a list of characters
+    char_list = list(input_string)
+    
+    # Shuffle the characters to create a random string
+    random.shuffle(char_list)
+    
+    # Join the shuffled characters to form the random string
+    random_string = ''.join(char_list)
+    return random_string
+
+def main():
+    input_string = input("Enter an input string: ")
+    
+    # Generate and print 5 random strings
+    for _ in range(5):
+        random_string = generate_random_string(input_string)
+        print(random_string)
+
+if __name__ == "__main__":
+    main()
+
+
+# Task 4
+
+import random
+
+def generate_math_problem():
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    return num1, num2, num1 + num2
+
+def main():
+    num1, num2, correct_answer = generate_math_problem()
+    print(f"Let's solve a math problem:")
+    print(f"What is {num1} + {num2}?")
+    
+    try:
+        user_answer = int(input("Enter your answer: "))
+        if user_answer == correct_answer:
+            print("Correct! You got it right.")
+        else:
+            print(f"Wrong answer. The correct answer is {correct_answer}.")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
